0db85f453a9f4913c1027737f6dc3fbbb303b12f
Kristian Peters <kristian.peters@korseby.net>
2020-07-28 16:10:26 +0200

    docker changes

--- galaxy/mFam_train_classifier.sh
+++ galaxy/mFam_train_classifier.sh
@@ -10,21 +10,21 @@ export mfamres=$4
 export mfaminfo=$5
 
 echo "INPUT"
-echo "    workingdir: /data/";
-echo "    classlib: $classlib";
-echo "    annofile: $annofile";
-echo "";
-echo "OUTPUT";
-echo "    outputdir: /data/data/Classifier_ROC_Analysis/";
-echo "    mfamrdata: $mfamrdata";
-echo "    mfamres: $mfamres";
-echo "    mfaminfo: $mfaminfo";
+echo "    workingdir: /data/"
+echo "    classlib: $classlib"
+echo "    annofile: $annofile"
+echo ""
+echo "OUTPUT"
+echo "    outputdir: /data/data/Classifier_ROC_Analysis/"
+echo "    mfamrdata: $mfamrdata"
+echo "    mfamres: $mfamres"
+echo "    mfaminfo: $mfaminfo"
 
-mkdir -p /data/data/Classifier_ROC_Analysis;
-mkdir -p /data/data/MetFamily_class_projects;
-mFam_train_classifier.r /data/ $classlib $annofile /data/data/Classifier_ROC_Analysis/;
-cp -f /data/data/Classifier_ROC_Analysis/*.RData $mfamrdata;
-cp -f /data/data/Classifier_ROC_Analysis/*.tsv $mfamres;
-cp -f /data/data/Classifier_ROC_Analysis/*.txt $mfaminfo;
+mkdir -p /data/data/Classifier_ROC_Analysis
+mkdir -p /data/data/MetFamily_class_projects
+mFam_train_classifier.r /data/ $classlib $annofile /data/data/Classifier_ROC_Analysis/
+cp -f /data/data/Classifier_ROC_Analysis/*.RData $mfamrdata
+cp -f /data/data/Classifier_ROC_Analysis/*.tsv $mfamres
+cp -f /data/data/Classifier_ROC_Analysis/*.txt $mfaminfo
 
 
